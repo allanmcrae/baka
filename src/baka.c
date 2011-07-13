a86161b972d1a4904a97b79c24494a1e491d2f93
Allan McRae <allan@archlinux.org>
2011-07-13 16:23:37 +1000

    Basic window in GTK+3
    Signed-off-by: Allan McRae <allan@archlinux.org>

---
+++ src/baka.c
@@ -0,0 +1,44 @@
+/*
+ *  baka.c
+ *
+ *  Copyright (c) 2011 Allan McRae <allan@archlinux.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtk/gtk.h>
+
+static void activate(GtkApplication *app)
+{
+	GtkWidget *window;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_application(GTK_WINDOW(window), app);
+	gtk_widget_show_all(window);
+}
+
+
+int main(int argc, char **argv)
+{
+	GtkApplication *app;
+	gint status;
+
+	app = gtk_application_new("am.i.baka", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
+
+	return status;
+}
